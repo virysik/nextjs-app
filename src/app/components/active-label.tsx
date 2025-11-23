96b489cc068533f3a1040646707bdbcbd09ca45a
virysik <68701496+virysik@users.noreply.github.com>
2025-11-23 19:28:20 +0100

    add clsx

--- src/app/components/active-label.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactNode } from 'react';
-import styles from './active-label.module.css';
-
-export interface ActiveLabelProps {
-  children: ReactNode;
-}
-export default function ActiveLabel({ children }: ActiveLabelProps) {
-  return <span className={styles.label}>{children}</span>;
-}
--- src/app/components/not-active-label.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactNode } from 'react';
-import styles from './not-active-label.module.css';
-
-export interface NotActiveLabelProps {
-  children: ReactNode;
-}
-export default function NotActiveLabel({ children }: NotActiveLabelProps) {
-  return <span className={styles.label}>{children}</span>;
-}
